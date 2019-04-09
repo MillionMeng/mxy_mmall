7aa5be05089cfeca3c6b25db7c877aba530138d5
mengxiangyu <514920622@qq.com>
2019-04-10 00:00:52 +0800

    fix bug

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -42,6 +42,7 @@ public class ProductManageController {
      * @return
      */
     @RequestMapping(value = "save.do",method = RequestMethod.POST)
+    @ResponseBody
     public ServerResponse productSave(HttpSession session, Product product){
         User user = (User)session.getAttribute(Const.CURRENT_USER);
         if(user == null){
@@ -132,10 +133,10 @@ public class ProductManageController {
     @ResponseBody
     public ServerResponse upload(HttpSession session,@RequestParam(value = "upload_file",required = false) MultipartFile file,HttpServletRequest request) {
         User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (user == null) {
+        /*if (user == null) {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
-        }
-        if (iUserService.checkAdmin(user).isSuccess()) {
+        }*/
+        //if (iUserService.checkAdmin(user).isSuccess()) {
             String path = request.getSession().getServletContext().getRealPath("upload");
             String targetFileName = iFileService.upload(file, path);
             String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
@@ -144,10 +145,10 @@ public class ProductManageController {
             fileMap.put("uri", targetFileName);
             fileMap.put("url", url);
             return ServerResponse.createBySuccess(fileMap);
-        } else {
+        } /*else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
-    }
+    }*/
 
 
 
